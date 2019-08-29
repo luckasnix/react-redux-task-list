0f9a71e7e08cd0da80291227a064b4126a450215
luckasnix <luc.kasnix@gmail.com>
2019-08-29 12:04:16 -0300

    Input focado

--- src/pages/components/ToDoInput.js
+++ src/pages/components/ToDoInput.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import styles from './ToDoInput.module.css'
 
 function ToDoInput(props) {
+  const inputRef = useRef(null)
+  useEffect(
+    () => {
+      console.log(inputRef)
+      inputRef.current.focus()
+    },
+    []
+  )
   return (
     <input
       className={styles.input}
+      ref={inputRef}
       type='text'
       placeholder={props.placeholder}
       value={props.value}
